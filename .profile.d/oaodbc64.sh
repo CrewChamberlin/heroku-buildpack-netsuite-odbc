deb6fadfce26e4ab1be2628e12353f63cc6ac21c
Oren Forer <oforer@poppin.com>
2021-04-13 16:31:01 -0400

    Init commit

---
+++ .profile.d/oaodbc64.sh
@@ -0,0 +1,5 @@
+LD_LIBRARY_PATH=${HOME}/.apt/usr/lib/netsuite/odbcclient/lib64${LD_LIBRARY_PATH:+":"}${LD_LIBRARY_PATH:-""}
+export LD_LIBRARY_PATH
+OASDK_ODBC_HOME=${HOME}/.apt/usr/lib/netsuite/odbcclient/lib64; export OASDK_ODBC_HOME
+ODBCINI=${HOME}/.apt/usr/lib/netsuite/odbcclient/odbc64.ini; export ODBCINI
+
---
+++ README.md
@@ -0,0 +1,14 @@
+heroku-buildpack-netsuite-odbc
+===
+
+## Usage
+```
+unix-odbc
+unixodbc-dev <-- required for ruby-odbc gem
+```
+
+Add buildpack
+
+```
+heroku buildpacks:add ...
+```
