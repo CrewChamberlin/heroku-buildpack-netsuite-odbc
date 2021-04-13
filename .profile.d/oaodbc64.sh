3fd1a6676cd88bed1550d87f3f2ce45602e83d03
Oren Forer <oforer@poppin.com>
2021-04-13 17:48:41 -0400

    In development

--- .profile.d/oaodbc64.sh
+++ .profile.d/oaodbc64.sh
@@ -1,5 +1,25 @@
-LD_LIBRARY_PATH=${HOME}/.apt/usr/lib/netsuite/odbcclient/lib64${LD_LIBRARY_PATH:+":"}${LD_LIBRARY_PATH:-""}
+LD_LIBRARY_PATH=${HOME}/.apt/usr/lib/netsuite/odbc/bin/lib64${LD_LIBRARY_PATH:+":"}${LD_LIBRARY_PATH:-""}
 export LD_LIBRARY_PATH
-OASDK_ODBC_HOME=${HOME}/.apt/usr/lib/netsuite/odbcclient/lib64; export OASDK_ODBC_HOME
-ODBCINI=${HOME}/.apt/usr/lib/netsuite/odbcclient/odbc64.ini; export ODBCINI
+OASDK_ODBC_HOME=${HOME}/.apt/usr/lib/netsuite/odbc/bin/lib64; export OASDK_ODBC_HOME
+ODBCINI=${HOME}/.apt/usr/lib/netsuite/odbc/bin/odbc64_custom.ini; export ODBCINI
 
+echo "[ODBC Data Sources]
+NetSuite=NetSuite ODBC Drivers 8.1
+
+[NetSuite]
+Driver=${HOME}/.apt/usr/lib/netsuite/odbc/bin/lib64/ivoa27.so
+Description=Connect to your NetSuite account
+Host=${NETSUITE_ACCOUNT_ID}.connect.api.netsuite.com
+Port=1708
+ServerDataSource=NetSuite.com
+Encrypted=1
+Truststore=${HOME}/.apt/usr/lib/netsuite/odbc/bin/cert/ca3.cer
+CustomProperties=AccountID=${NETSUITE_ACCOUNT_ID};RoleID=3
+
+[ODBC]
+Trace=0
+IANAAppCodePage=4
+TraceFile=odbctrace.out
+TraceDll=${HOME}/.apt/usr/lib/netsuite/odbc/bin/lib64/ddtrc27.so
+InstallDir=${HOME}/.apt/usr/lib/netsuite/odbc/bin
+" > ${HOME}/.apt/usr/lib/netsuite/odbc/bin/odbc64_custom.ini
